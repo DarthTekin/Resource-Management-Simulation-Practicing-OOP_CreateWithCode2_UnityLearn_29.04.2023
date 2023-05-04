6fc26671025912b03de2cec7ce34116eb1949dd4
DarthTekin <tekinteks@gmail.com>
2023-05-04 18:26:29 +0300

    ResourcePile and ProductivityMultiplier
    ResourcePile and ProductivityMultiplier variables added in ProductivityUnit.cs

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplyer = 2;
+
     protected override void BuildingInRange()
     {
 
